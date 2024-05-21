12b18602f3fb3101e611a596e785a0509e142c6e
twonezero <dnjsdudtm98@naver.com>
2024-05-21 20:46:20 +0900

    템플릿 메서드 패턴 적용

---
+++ src/test/java/hello/advanced/template/TemplateMethodTest.java
@@ -0,0 +1,70 @@
+package hello.advanced.template;
+
+import hello.advanced.template.code.AbstractTemplate;
+import hello.advanced.template.code.SubClassLogic1;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직 1 실행");
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직 2 실행");
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    // 템플릿 메서드 적용 -> 단일 책임 원칙을 지킬 수 있음
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic1();
+        template2.execute();
+    }
+
+    // 템플릿 메서드는 sub클래스를 여러개 만들어야할 수 도 있으므로
+    // 익명 내부 클래스로 만들기
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직 1 실행");
+            }
+        };
+        log.info("클래스 이름1 = {}", template1.getClass());
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직 2 실행");
+            }
+        };
+        log.info("클래스 이름2 = {}", template1.getClass());
+        template1.execute();
+        template2.execute();
+
+    }
+
+}
