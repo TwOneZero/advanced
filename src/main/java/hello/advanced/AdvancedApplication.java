c90afa65aca9f1cec0719001e3b83405395184fb
twonezero <dnjsdudtm98@naver.com>
2024-05-24 22:30:14 +0900

    동적 프록시 : 인터페이스와 구현 객체를 통한 생성

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -4,6 +4,8 @@ import hello.advanced.config.AppV1Config;
 import hello.advanced.config.AppV2Config;
 import hello.advanced.config.v1_proxy.ConcreteProxyConfig;
 import hello.advanced.config.v1_proxy.InterfaceProxyConfig;
+import hello.advanced.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.advanced.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.advanced.trace.logtrace.LogTrace;
 import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -12,7 +14,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import({AppV1Config.class, AppV2Config.class})
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+@Import(DynamicProxyBasicConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.advanced.app.v3")
 public class AdvancedApplication {
 
